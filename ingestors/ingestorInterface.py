4d6e7a28a2dc840bef1fc9bc0741fe81c5647120
Jeannie An <jeanniehjan@gmail.com>
2021-01-17 18:29:38 +0000

    ingestors done

--- ingestors/ingestorInterface.py
+++ ingestors/ingestorInterface.py
@@ -1,4 +1,9 @@
 from models import QuoteModel
+from docx import Document
+
+import csv
+import os
+import subprocess
 
 
 class IngestorInterface:
@@ -17,8 +22,8 @@ class TextIngestor(IngestorInterface):
         res = []
         file = open(path, mode='r', encoding='utf-8-sig')
         lines = file.readlines()
-        for line in lines:
-            res.append(QuoteModel(line.rstrip('\n').split(' - ')))
+        for row in lines:
+            res.append(QuoteModel(row.rstrip('\n').split(' - ')))
         file.close()
         return res
 
@@ -26,16 +31,37 @@ class TextIngestor(IngestorInterface):
 class CSVIngestor(IngestorInterface):
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
-        pass
+        res = []
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                    continue
+                else:
+                    res.append(QuoteModel(row.rstrip('\n').split(' - ')))
+                    line_count += 1
+            return res
 
 
 class DocxIngestor(IngestorInterface):
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
-        pass
+        res = []
+        document = Document(path)
+        for paragraph in document.paragraphs:
+            paragraph.text and res.append(
+                QuoteModel(paragraph.text.split(" - ")))
+        return res
 
 
 class PDFIngestor(IngestorInterface):
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
-        pass
+        text_file = "./resource/temp.txt"
+        cmd = f"./resource/pdftotext -layout -nopgbrk {path} {text_file}"
+        subprocess.call(cmd, shell=True, stderr=subprocess.STDOUT)
+        res = TextIngestor.parse(text_file)
+        os.remove(text_file)
+        return res
